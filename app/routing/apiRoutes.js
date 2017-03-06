63d7a68b0a103cbae93b900c2745205c2750dd43
Deepika MUSUVATHY <Deepika MUSUVATHY>
2017-03-06 17:19:32 -0500

    Difference in score logic sorted

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,43 +1,43 @@
-var friendsList=require("../data/friends.js");
+var friendsList = require("../data/friends.js");
 
-module.exports=function(app){
-	app.get("/api/friends",function(req,res){
-		res.json(friendsList);
-	})
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsList);
+    })
 
-	app.post("/api/friends",function(req,res){
-		var matchedFriend={
-			name:"",
-			photo:"",
-			difference:1000
-		}
-		console.log(req.body);
+    app.post("/api/friends", function(req, res) {
+        var matchedFriend = {
+            name: "",
+            photo: "",
+            difference: 1000
+        }
+        console.log(req.body);
 
-		var userDetails=req.body;
-		var userScores=userDetails.scores;
+        var userDetails = req.body;
+        var userScores = userDetails.scores;
 
-		console.log(userScores);
+        console.log(userScores);
 
-		var scoreDifference=0;
-		for(let i=0;i<friendsList.length;i++){
-			console.log(friendsList[i]);
-			scoreDifference=0;
+        var scoreDifference = 0;
+        for (let i = 0; i < friendsList.length; i++) {
+            console.log(friendsList[i]);
+            scoreDifference = 0;
 
-			for(let j=0;j<friendsList[i].scores[j];j++){
-				scoreDifference=Math.abs(parseInt(userScores[j]-parseInt(friendsList[i].scores[j])));
-				if(scoreDifference<=matchedFriend.difference){
-					matchedFriend.name=friendsList[i].name;
-					matchedFriend.photo=friendsList[i].photo;
-					matchedFriend.difference=scoreDifference;
-				}
+            for (let j = 0; j < 9; j++) {
+                scoreDifference =scoreDifference+(Math.abs(parseInt(userScores[j]) - parseInt(friendsList[i].scores[j])));
+            }
 
+            console.log(scoreDifference);
 
-			}
+            if (scoreDifference <= matchedFriend.difference) {
+                matchedFriend.name = friendsList[i].name;
+                matchedFriend.photo = friendsList[i].photo;
+                matchedFriend.difference = scoreDifference;
+            }
 
+        }
+        friendsList.push(userDetails);
+        res.json(matchedFriend);
+    });
 
-		}
-
-		friendsList.push(userDetails);
-		res.json(matchedFriend);
-	})
 }
